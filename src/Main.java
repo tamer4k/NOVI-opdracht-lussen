6093be60366968560b683c8c2c3d2d62665d8105
tamer4k <tamer.alashraf@hotmail.com>
2023-10-02 17:33:47 +0200

    commit message

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
-        System.out.println("hello 4k");
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
+        Translator translator = new Translator(alphabetic, numeric);
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
+        while (play) {
+            System.out.println("Type 'x' om te stoppen\nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                scanner.nextLine(); // Leeg de invoerbuffer
 
+                if (number >= 0 && number <= 9) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println(ongeldig);
+                }
+            } else {
+                System.out.println(ongeldig);
+            }
+        }
     }
 }
---
+++ src/Translator.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class Translator {
+    HashMap<Integer, String> numericAlpha = new HashMap<>();;
+
+    public Translator(String[] alphabetic, Integer[] numeric) {
+        for (int i = 0; i < alphabetic.length; i++) {
+            numericAlpha.put(numeric[i], alphabetic[i]);
+        }
+    }
+
+    public String translate(Integer number) {
+        return numericAlpha.get(number);
+    }
+
+}
